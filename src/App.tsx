9482609d4590d6c3ad964e8e9a9af581c07b6a3f
CarboniDavide <31761218+CarboniDavide@users.noreply.github.com>
2026-08-27 00:40:54 +0200

    use another provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import { LanguageProvider } from './contexts/LanguageContext'
-import { useWakeup } from './hooks/useWakeup'
 import { AppRoutes } from './routes/AppRoutes'
 
 function App() {
-  useWakeup()
-
   return (
     <LanguageProvider>
       <BrowserRouter>
