ee0e5a7646122fc2df0bf677c8fc6879c87763a2
CarboniDavide <31761218+CarboniDavide@users.noreply.github.com>
2026-08-28 00:54:36 +0200

    allow to have logs in local dev

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
   server: {
+    proxy: {
+      '/api': {
+        target: 'http://host.docker.internal:3000',
+        changeOrigin: true,
+      },
+    },
     watch: {
       // Docker on Windows doesn't forward inotify events — polling is required
       usePolling: true,
